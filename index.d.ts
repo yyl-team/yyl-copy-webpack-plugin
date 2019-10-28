c7be075472505a41beca617a6d241f916600ff74
liudaojie <liudaojie@yy.com>
2019-10-28 22:03:00 +0800

    feat: 添加 hooks

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,21 @@
+import { AsyncSeriesWaterfallHook } from 'tapable'
+
+interface Hooks {
+  beforeCopy: AsyncSeriesWaterfallHook<{
+    src: string,
+    dist: string,
+    source: string
+  }>
+  afterCopy: AsyncSeriesWaterfallHook<{
+    src: string,
+    dist: string,
+    source: string
+  }>
+}
+
 declare class IWebpackPlugin {
+  static getName(): string
+  static getHooks(): iHooks
   constructor(options : ICopyOption[])
 }
 interface ICopyOption {
